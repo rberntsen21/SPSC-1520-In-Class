e8ece3a6aa1e7fd1939f9f07dbc9ce0e3486960a
rberntsen21 <35229176+rberntsen21@users.noreply.github.com>
2018-01-22 08:23:18 -0700

    Added handleClickFeature to javascript

--- week03/events-intro/js/main.js
+++ week03/events-intro/js/main.js
@@ -1,5 +1,12 @@
 // 1. select the target element
-
+var featureImage = document.querySelector('img.feature');
 // 2. create the event listener function
-
+function handleFeatureClick() {
+    console.log('img.feature was clicked...');
+    // Grab the HTML element with a .feature and a .description class
+    var desc = document.querySelector('.feature.description');
+    // All HTML elemnt objects have a .class list property that is a colection of class names.
+    desc.classList.remove('hidden');
+}    
 // 3. add event listener
+featureImage.addEventListener('click',handleFeatureClick);
