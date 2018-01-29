f8bdf38143e05207ff6ed13eed88f4ddb44fb06a
rberntsen21 <35229176+rberntsen21@users.noreply.github.com>
2018-01-29 09:04:43 -0700

    Cleaned up code. Used anonymous function

--- week04/decisions-intro/js/main.js
+++ week04/decisions-intro/js/main.js
@@ -1,26 +1,13 @@
-// 1. select the target element
-var featureImage = document.querySelector('img.feature');
+// An anonymous function is a function that does not have a name.
+// Anonymous functions are often used when you want to create a "one-off"
+// function that is being passed into another method/function.
+document.querySelector('img.feature')
+	.addEventListener('click', function () {
+		var desc = document.querySelector('p.feature.description');
 
-// 2. create the event listener function
-function handleFeatureClick() {
-	var desc = document.querySelector('p.feature.description');
-
-	// remove the hidden class IF it is present,
-	// otherwise, add it back!
-	//if(desc.classList.contains('hidden')) {
-	//	desc.classList.remove('hidden');
-	//} else {
-	//	desc.classList.add('hidden');
-	//}
-	
-	// achieve the same result using using string functions
-	if(desc.className.indexOf('hidden') >= 0) {
+		if(desc.classList.contains('hidden')) {
 			desc.classList.remove('hidden');
-	} else {
+		} else {
 		desc.classList.add('hidden');
-	}
-}
-
-// 3. add event listener
-featureImage.addEventListener('click', handleFeatureClick);
-
+		}
+	});
