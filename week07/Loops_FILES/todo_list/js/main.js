781d2774e189802bf3e4c3c97bacc1197a04edd3
rberntsen21 <35229176+rberntsen21@users.noreply.github.com>
2018-02-26 09:20:32 -0700

    Added Loop Intro Documents

---
+++ week07/Loops_FILES/todo_list/js/main.js
@@ -0,0 +1,16 @@
+// Enter JavaScript for the exercise here...
+document.querySelector('[name=toggle]').addEventListener('change', function(evt){
+    var checks = document.querySelectorAll('[name=message]');
+    var count = 0;
+    
+    while(count < checks.length){
+        if(evt.target.checked){
+            checks[count].checked = true;
+        } else{
+            checks[count].checked = false
+        }
+
+        count = count +1;
+    }
+
+});
